8a124978ebbcf1c9f3e01138e60e0337a8c9cc29
Michal Cupák <michal.cupak@ceitec.muni.cz>
2025-10-30 16:46:50 +0100

    funkcni aplikace

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [shuffledImages, setShuffledImages] = useState([]);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  // useEffect(() => {
+  //   const shuffledUrls = [...imageUrls].sort(() => Math.random() - 0.5);
+  //   setShuffledImageUrls(shuffledUrls);
+  // }, []);
+
+  useEffect(() => {
+    const fetchImages = async () => {
+      try {
+        const response = await fetch(`${process.env.PUBLIC_URL}/image_urls.json?t=${Date.now()}`);
+        const images = await response.json();
+        const shuffled_images = [...images].sort(() => Math.random() - 0.5);
+        setShuffledImages(shuffled_images);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchImages();
+  }, []);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % shuffledImages.length);
+    }, 10000);
+
+    return () => clearInterval(intervalId);
+  }, [shuffledImages.length]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="fullscreen-background">
+        {shuffledImages.map((image, index) => (
+            <div key={index} className={`slide-container ${index === currentIndex ? 'active' : ''}`}>
+              <div
+                  className="slide"
+                  style={{
+                    backgroundImage: `url(${image.url})`,
+                    backgroundSize: 'contain',
+                    backgroundPosition: 'center',
+                    backgroundRepeat: 'no-repeat',
+                    width: '100%',
+                    height: 'calc(100vh - 20px)',
+                  }}
+              />
+              <div className="slide-description">
+                {image.pilot}, {image.datum}
+              </div>
+            </div>
+        ))}
+      </div>
   );
 }
 
